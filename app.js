00ed04a3666ffb9f6675a4bb4cf1592802d9aa75
Abhishek Singh <abhishek.skd13@gmail.com>
2024-05-18 13:00:53 +0530

    final commit

--- app.js
+++ app.js
@@ -3,6 +3,9 @@
 // document.querySelector(".slider").addEventListener('input', (e) => {
 //     container.style.setProperty('--position', ${e.target.value}%);
 // })
+
+// ${e.target.value}% error at line no. 4
+
 const container = document.querySelector(".container");
 
 document.querySelector(".slider").addEventListener('input', (e) => {
