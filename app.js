8113ab9214c2b9137512f08451686d6939c91d0c
Abhishek Singh <abhishek.skd13@gmail.com>
2024-05-18 12:38:24 +0530

    second js updated

--- app.js
+++ app.js
@@ -1,5 +1,10 @@
+// const container = document.querySelector(".container");
+
+// document.querySelector(".slider").addEventListener('input', (e) => {
+//     container.style.setProperty('--position', ${e.target.value}%);
+// })
 const container = document.querySelector(".container");
 
 document.querySelector(".slider").addEventListener('input', (e) => {
-    container.style.setProperty('--position', ${e.target.value}%);
-})
+    container.style.setProperty('--position', `${e.target.value}%`);
+});
