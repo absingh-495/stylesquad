5d15e82d441471e177ea66bd7cb5e5ff530ec227
Abhishek Singh <abhishek.skd13@gmail.com>
2024-06-20 13:52:48 +0530

    image added

--- app.js
+++ app.js
@@ -1,11 +1,3 @@
-// const container = document.querySelector(".container");
-
-// document.querySelector(".slider").addEventListener('input', (e) => {
-//     container.style.setProperty('--position', ${e.target.value}%);
-// })
-
-// ${e.target.value}% error at line no. 4
-
 const container = document.querySelector(".container");
 
 document.querySelector(".slider").addEventListener('input', (e) => {
