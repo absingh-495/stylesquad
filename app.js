9848aaeb1d0a1ae757e0deec1fac54826fb6305e
Abhishek Singh <abhishek.skd13@gmail.com>
2024-06-23 12:45:53 +0530

    updated

--- app.js
+++ app.js
@@ -1,7 +1,8 @@
-const container = document.querySelector(".container");
+const container = document.allquerySelector(".container");
 
 document.querySelector(".slider").addEventListener('input', (e) => {
     container.style.setProperty('--position', `${e.target.value}%`);
 });
+
 
 
